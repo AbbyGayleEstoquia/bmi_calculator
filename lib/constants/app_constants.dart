c66570858f17c3843e9df14c502a68dbc686d219
AbbyGayleEstoquia <abbygaylestalk@gmail.com>
2021-09-04 16:23:10 +0800

    first commit

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#211522");
+final Color accentHexColor = HexColor("#BBE7FE");
